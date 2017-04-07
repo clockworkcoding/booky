1b47865568bcbdb1aec0d540db8e989d027d7832
clockworkcoding <yodadamax@gmail.com>
2017-04-06 22:37:30 -0500

    Logging errors shouldn't crash the app

--- booky.go
+++ booky.go
@@ -29,11 +29,10 @@ var globalState state
 
 // writeError writes an error to the reply - example only
 func writeError(w http.ResponseWriter, status int, err string) {
-	fmt.Printf("Err: %s", err)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write([]byte(err))
-	log.Fatal(fmt.Sprintf("Err: %s", err))
+	log.Output(1, fmt.Sprintf("Err: %s", err))
 }
 
 func responseError(responseURL, message, token string) {
@@ -50,7 +49,7 @@ func simpleResponse(responseURL, message, token string) {
 	api := slack.New(token)
 	err := api.PostResponse(responseURL, params)
 	if err != nil {
-		log.Output(1, fmt.Sprintf("Err: %s", err.Error))
+		log.Output(0, fmt.Sprintf("Err: %s", err.Error()))
 	}
 
 }
